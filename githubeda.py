5a8eab8884eb13c2d03a47c2f67c09fe70405293
Deeruuhuhu <rdeeraja@gmail.com>
2026-04-22 07:32:37 +0530

    Refactor code structure and remove comments

--- githubeda.py
+++ githubeda.py
@@ -1,14 +1,10 @@
-# ============================================
-# GITHUB TOP REPOSITORIES - EDA
-# ============================================
 
-# ---------- Import Libraries ----------
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-# ---------- Load Dataset ----------
+
 df = pd.read_csv(r"C:\Users\HP\Downloads\github_top_repositories.csv")   # change file name if needed
 
 print("\nFirst 5 Rows:\n", df.head())
